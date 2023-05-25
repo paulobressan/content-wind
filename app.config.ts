e6c2a99dfc6191787c2cf8a721854314e09d6f19
paulo.bressan <paulo.bressan@outlook.com>
2023-05-24 21:16:27 -0300

    feat: added index texts

--- app.config.ts
+++ app.config.ts
@@ -2,8 +2,8 @@ export default defineAppConfig({
   // Configure Content Wind here
   cover: '/cover.jpg',
   socials: {
-    twitter: 'Atinux',
-    github: 'Atinux/content-wind'
+    twitter: 'Paulo_BressanJr',
+    github: 'paulobressan'
   },
   prose: {
     h1: {
